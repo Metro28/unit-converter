b65aacd2b9d2cb6d7d3ec3cf03d9aa8aaf9d06c3
Metro28 <87281532+Metro28@users.noreply.github.com>
2025-08-23 01:43:35 -0300

    Add files via upload

--- convertidorsource.pas
+++ convertidorsource.pas
@@ -43,27 +43,43 @@ begin
   ComboBox3.Items.Clear;
 
   case ComboBox1.ItemIndex of
-  0: begin // Longitud
+  0: begin // Energía
+    ComboBox2.Items.AddStrings(['Julio','Kilojulio','Caloría-gramo','Kilocaloría','Vatio-hora','Kilovatio-hora','Electrón-voltio','Unidad térmica británica','Termia estadounidense','Pie-libra fuerza']);
+    ComboBox3.Items.AddStrings(ComboBox2.Items);
+    end;
+
+  1: begin
+    ComboBox2.Items.AddStrings(['Hercio (Hz)','Kilohercio (kHz)','Megahercio (MHz)','Gigahercio (GHz)']);
+    ComboBox3.Items.AddStrings(ComboBox2.Items);
+    end;
+
+  2: begin // Longitud
     ComboBox2.Items.AddStrings(['Kilómetros','Metros','Centímetros','Milímetros','Millas','Pies','Pulgadas']);
     ComboBox3.Items.AddStrings(ComboBox2.Items);
     end;
 
-  1: begin // Tiempo
+  3: begin // Masa
+    ComboBox2.Items.AddStrings(['Toneladas','Kilogramos','Gramos','Miligramos','Microgramos','Libras']);
+    ComboBox3.Items.AddStrings(ComboBox2.Items);
+    end;
+
+  4: begin // Temperatura
+    ComboBox2.Items.AddStrings(['Celcius','Fahrenheit','Kelvin']);
+    ComboBox3.Items.AddStrings(ComboBox2.Items);
+    end;
+
+  5: begin // Tiempo
     ComboBox2.Items.AddStrings(['Horas','Minutos','Segundos']);
     ComboBox3.Items.AddStrings(ComboBox2.Items);
     end;
 
-  2: begin // Masa
-    ComboBox2.Items.AddStrings(['Toneladas','Kilogramos','Gramos','Miligramos','Libras']);
+  6: begin // Velocidad
+    ComboBox2.Items.AddStrings(['Kilómetros por hora', 'Metros por segundo', 'Millas por hora', 'Pies por segundo', 'Nudos']);
     ComboBox3.Items.AddStrings(ComboBox2.Items);
     end;
 
-  3: begin // Temperatura
-    ComboBox2.Items.AddStrings(['Celcius','Fahrenheit','Kelvin']);
-    ComboBox3.Items.AddStrings(ComboBox2.Items);
-    end;
-  4: begin // Velocidad
-    ComboBox2.Items.AddStrings(['Kilómetros por hora', 'Metros por segundo', 'Millas por hora', 'Pies por segundo', 'Nudos']);
+  7: begin // Capacitancia
+    ComboBox2.Items.AddStrings(['Faradio','Milifaradio','Microfaradio','Nanofaradio','Picofaradio','Femtofaradio']);
     ComboBox3.Items.AddStrings(ComboBox2.Items);
     end;
   end;
@@ -83,8 +99,50 @@ begin
 
   case ComboBox1.ItemIndex of
 
-  // Longitud
+  // Energía
   0: begin
+    // pasar todo a Julios
+    if unidadDe = 'Julio' then valor := valor
+    else if unidadDe = 'Kilojulio' then valor := valor * 1000
+    else if unidadDe = 'Caloría-gramo' then valor := valor * 4.184
+    else if unidadDe = 'Kilocaloría' then valor := valor * 4184
+    else if unidadDe = 'Vatio-hora' then valor := valor * 3600
+    else if unidadDe = 'Kilovatio-hora' then valor := valor * 3.6e6
+    else if unidadDe = 'Electrón-voltio' then valor := valor * 1.602176634e-19
+    else if unidadDe = 'Unidad térmica británica' then valor := valor * 1055.05505
+    else if unidadDe = 'Termia estadounidense' then valor := valor * 1.055e8
+    else if unidadDe = 'Pie-libra fuerza' then valor := valor * 1.35581795;
+
+    // de Julios a destino
+    if unidadA = 'Julio' then resultado := valor
+    else if unidadA = 'Kilojulio' then resultado := valor / 1000
+    else if unidadA = 'Caloría-gramo' then resultado := valor / 4.184
+    else if unidadA = 'Kilocaloría' then resultado := valor / 4184
+    else if unidadA = 'Vatio-hora' then resultado := valor / 3600
+    else if unidadA = 'Kilovatio-hora' then resultado := valor / 3.6e6
+    else if unidadA = 'Electrón-voltio' then resultado := valor / 1.602176634e-19
+    else if unidadA = 'Unidad térmica británica' then resultado := valor / 1055.05505
+    else if unidadA = 'Termia estadounidense' then resultado := valor / 1.055e8
+    else if unidadA = 'Pie-libra fuerza' then resultado := valor / 1.35581795;
+  end;
+
+  // Frecuencia
+  1: begin
+    // pasar todo a Hz
+    if unidadDe = 'Hercio (Hz)' then valor := valor
+    else if unidadDe = 'Kilohercio (kHz)' then valor := valor * 1000
+    else if unidadDe = 'Megahercio (MHz)' then valor := valor * 1000000
+    else if unidadDe = 'Gigahercio (GHz)' then valor := valor * 1000000000;
+
+    // de Hz a destino
+    if unidadA = 'Hercio (Hz)' then resultado := valor
+    else if unidadA = 'Kilohercio (kHz)' then resultado := valor / 1000
+    else if unidadA = 'Megahercio (MHz)' then resultado := valor / 1000000
+    else if unidadA = 'Gigahercio (GHz)' then resultado := valor / 1000000000;
+  end;
+
+  // Longitud
+  2: begin
     if unidadDe = 'Kilómetros' then valor := valor * 1000
     else if unidadDe = 'Centímetros' then valor := valor / 100
     else if unidadDe = 'Milímetros' then valor := valor / 1000
@@ -101,32 +159,24 @@ begin
     else resultado := valor;
   end;
 
-  // Tiempo
-  1: begin
-    if unidadDe = 'Horas' then valor := valor * 3600
-    else if unidadDe = 'Minutos' then valor := valor * 60;
-
-    if unidadA = 'Horas' then resultado := valor / 3600
-    else if unidadA = 'Minitos' then resultado := valor / 60
-    else resultado := valor;
-  end;
-
   // Masa
-  2: begin
+  3: begin
     if unidadDe = 'Toneladas' then valor := valor * 1000
     else if unidadDe = 'Gramos' then valor := valor / 1000
     else if unidadDe = 'Miligramos' then valor := valor / 1000000
+    else if unidadDe = 'Microgramos' then valor := valor / 1000000000
     else if unidadDe = 'Libras' then valor := valor * 0.453592;
 
     if unidadA = 'Toneladas' then resultado := valor / 1000
     else if unidadA = 'Gramos' then resultado := valor * 1000
     else if unidadA = 'Miligramos' then resultado := valor * 1000000
+    else if unidadA = 'Microgramos' then resultado := valor * 1000000000
     else if unidadA = 'Libras' then resultado := valor / 0.453592
     else resultado := valor;
   end;
 
   // Temperatura
-  3: begin
+  4: begin
     if unidadDe = 'Fahrenheit' then valor := (valor - 32) * 5/9
     else if unidadDe = 'Kelvin' then valor := valor - 273.15;
 
@@ -135,8 +185,18 @@ begin
     else resultado := valor;
   end;
 
+  // Tiempo
+  5: begin
+    if unidadDe = 'Horas' then valor := valor * 3600
+    else if unidadDe = 'Minutos' then valor := valor * 60;
+
+    if unidadA = 'Horas' then resultado := valor / 3600
+    else if unidadA = 'Minitos' then resultado := valor / 60
+    else resultado := valor;
+  end;
+
   // Velocidad
-  4: begin
+  6: begin
     if unidadDe = 'Kilómetros por hora' then valor := valor * 0.277778
     else if unidadDe = 'Millas por hora' then valor := valor * 0.44704
     else if unidadDe = 'Pies por segundo' then valor := valor * 0.3048
@@ -146,6 +206,24 @@ begin
     else if unidadA = 'Millas por hora' then resultado := valor / 0.44704
     else if unidadA = 'Pies por segundo' then resultado := valor / 0.3048
     else if unidadA = 'Nudos' then resultado := valor / 0.514444
+    else resultado := valor;
+  end;
+
+  // Capacitancia
+  7: begin
+    if unidadDe = 'Faradio' then valor := valor
+    else if unidadDe = 'Milifaradio' then valor := valor * 1E-3
+    else if unidadDe = 'Microfaradio' then valor := valor * 1E-6
+    else if unidadDe = 'Nanofaradio' then valor := valor * 1E-9
+    else if unidadDe = 'Picofaradio' then valor := valor * 1E-12
+    else if unidadDe = 'Femtofaradio' then valor := valor * 1E-15;
+
+    if unidadA = 'Faradio' then resultado := valor
+    else if unidadA = 'Milifaradio' then resultado := valor / 1E-3
+    else if unidadA = 'Microfaradio' then resultado := valor / 1E-6
+    else if unidadA = 'Nanofaradio' then resultado := valor / 1E-9
+    else if unidadA = 'Picofaradio' then resultado := valor / 1E-12
+    else if unidadA = 'Femtofaradio' then resultado := valor / 1E-15
     else resultado := valor;
   end;
 end;
